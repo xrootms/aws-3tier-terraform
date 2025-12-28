853c10c104c44eff406d86ec130944b118a811de
saif25 <saif25@gmail.com>
2025-12-28 23:05:54 +0530

    feat: Add Terraform EC2 module for Application

---
+++ ec2/main.tf
@@ -0,0 +1,33 @@
+variable "ami_id" {}
+variable "instance_type" {}
+variable "ec2_tag_name" {}
+variable "key_name" {}
+variable "subnet_id" {}
+variable "sg_for_ec2" {}
+variable "enable_public_ip_address" {}
+variable "user_data_install_app" {}
+
+output "app_ec2_instance_id" {
+  value = aws_instance.srl_proj_dev_ec2.id
+}
+
+output "app_ec2_instance_public_ip" {
+  value = aws_instance.srl_proj_dev_ec2.public_ip
+}
+
+resource "aws_instance" "srl_proj_dev_ec2" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags = { Name = var.ec2_tag_name }
+
+    key_name                    = var.key_name
+    subnet_id                   = var.subnet_id
+    vpc_security_group_ids      = var.sg_for_ec2
+    associate_public_ip_address = var.enable_public_ip_address
+    user_data = var.user_data_install_app
+
+    metadata_options {
+    http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
+    http_tokens   = "required" # Require the use of IMDSv2 tokens
+  }
+}
